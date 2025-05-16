a449c1837533a72681a8b0e6e52c685c4416c21d
mertyavas00 <mertyavas000@gmail.com>
2025-05-16 20:43:09 +0300

    frontend tarafi Socket.io baglantisi guncellendi

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import io from "socket.io-client";
 import Chat from "./components/Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://realtime-chat-app-react.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
